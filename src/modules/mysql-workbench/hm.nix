b43753d96163a889e0ec2043496d0c3d6e0c959b
Luk-ESC <eschbacher.lukas@gmail.com>
2026-04-29 20:34:22 +0200

    feat(modules-mysql-workbench): add autoEnable

--- src/modules/mysql-workbench/hm.nix
+++ src/modules/mysql-workbench/hm.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ pkgs, config, ... }:
 {
   name = "mysql-workbench";
+  autoEnable = builtins.elem pkgs.mysql-workbench config.home.packages;
   cfg.persist = {
     session.contents = [ ".mysql/workbench/" ];
     logs.contents = [ ".mysql/workbench/log/" ];
