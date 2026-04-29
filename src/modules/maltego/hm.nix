a9f64c1b68025f9367e6e8b25008091817450e94
Luk-ESC <eschbacher.lukas@gmail.com>
2026-04-29 20:37:35 +0200

    feat(modules-maltego): add autoEnable

--- src/modules/maltego/hm.nix
+++ src/modules/maltego/hm.nix
@@ -1,13 +1,11 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, config, ... }:
 let
   base = ".maltego/";
   versionPath = "${base}v${pkgs.maltego.version}";
 in
 {
   name = "maltego";
+  autoEnable = builtins.elem pkgs.maltego config.home.packages;
   cfg.persist = {
     logs.contents = [ "${versionPath}/var/log/" ];
     caches.contents = [ "${versionPath}/var/cache/" ];
