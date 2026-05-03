93625bd06978712a852de75f0201a93a58278fca
Luk-ESC <eschbacher.lukas@gmail.com>
2026-05-03 23:23:06 +0200

    feat(modules-go): set extraPaths

--- src/modules/go/hm.nix
+++ src/modules/go/hm.nix
@@ -1,10 +1,11 @@
 { config, pkgExists, ... }:
 let
-  # TODO: extraPaths?
+  # TODO: realPath?
   goPath = config.persist.caches.prefix + "caches" + config.home.homeDirectory + "/go/";
 in
 rec {
   name = "go";
   autoEnable = pkgExists name;
   cfg.home.sessionVariables.GOPATH = goPath;
+  cfg.atlas.extraPaths = [ goPath ];
 }
