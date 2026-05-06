edf65f320c8ad3221399c3cb1dd96b4156e152ad
Luk-ESC <eschbacher.lukas@gmail.com>
2026-05-06 14:28:29 +0200

    feat(modules): add xdg

---
+++ src/modules/xdg/hm.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+let
+  dirs = [
+    "desktop"
+    "documents"
+    "music"
+    "pictures"
+    "publicShare"
+    "templates"
+    "videos"
+  ];
+  absolutePaths = builtins.filter (x: x != null) (map (x: config.xdg.userDirs.${x}) dirs);
+  relPaths = map (lib.removePrefix "${config.home.homeDirectory}/") absolutePaths;
+  paths = map (x: x + "/") relPaths;
+in
+{
+  name = "xdg";
+  autoEnable = config.xdg.userDirs.enable;
+  cfg.persist.data.contents = paths;
+}
