9d76f8ca8a799f4dc5fe7582917a5782e508546a
Luk-ESC <eschbacher.lukas@gmail.com>
2026-06-08 09:46:54 +0200

    feat(modules-cargo): use hm module

--- src/modules/cargo/hm.nix
+++ src/modules/cargo/hm.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ config, ... }:
+let
+  # TODO: realPath?
+  cargoHome = config.persist.data.prefix + "caches" + config.home.homeDirectory + "/.cargo";
+in
 {
   name = "cargo";
-  cfg.persist = {
-    session.contents = [ ".cargo/" ];
-    caches.contents = [ ".cargo/registry/cache/" ];
-  };
+  autoEnable = config.programs.cargo.enable;
+  cfg.home.sessionVariables.CARGO_HOME = cargoHome;
+  cfg.atlas.extraPaths = [ cargoHome ];
 }
